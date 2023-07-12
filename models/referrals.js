f6a5931db6a06d52c86f99e29144b37e0c0babbf
omartamer-git <omartamer02@hotmail.com>
2023-07-12 17:10:07 +0300

    Referral

---
+++ models/referrals.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('Referral', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    expiry: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: () => {
+        const today = new Date();
+        const futureDate = new Date(today.getTime() + 30 * 24 * 60 * 60 * 1000); // Add 30 days to today
+        return futureDate;
+      },
+    },
+    ReferrerID: {
+      type: DataTypes.INTEGER,
+    },
+    RefereeID: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      validate: {
+        isGreaterThanReferrer(value) {
+          if (value <= this.ReferrerID) {
+            throw new Error('RefereeID must be greater than ReferrerID');
+          }
+        },
+      },
+    }
+  }, {
+    sequelize,
+    timestamps: true,
+    indexes: [
+      {
+        name: 'PRIMARY',
+        unique: true,
+        using: 'BTREE',
+        fields: [
+          { name: 'id' },
+        ],
+      },
+    ],
+  });
+};
