2da76d9c185c85314a6a6559636fbdd705470df2
omartamer-git <omartamer02@hotmail.com>
2023-08-25 20:28:51 +0300

    Fixed one more error

--- models/driverenrollments.js
+++ models/driverenrollments.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('Announcement', {
+    return sequelize.define('DriverEnrollment', {
         id: {
             autoIncrement: true,
             type: DataTypes.INTEGER,
