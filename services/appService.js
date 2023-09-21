380b9ad33145f2b0073a6e5f293183f9ac37432d
omartamer-git <omartamer02@hotmail.com>
2023-09-21 23:24:39 +0300

    Registering for SNS

--- services/appService.js
+++ services/appService.js
@@ -1,9 +1,17 @@
 const { Announcement, DriverEnrollment } = require("../models")
 const { NotFoundError } = require("../errors/Errors")
 
+const AWS = require('aws-sdk');
+AWS.config.update({
+    accessKeyId: 'AKIA4WPNBKF4XUVMTRE4',
+    secretAccessKey: 'fx6W1HLoNx/K1y9zrEKW6sGpXaerrYLzmu1iQt6+',
+    region: 'eu-central-1',  // e.g., us-west-2
+});
+const sns = new AWS.SNS();
+
 async function getAnnouncement(announcementId) {
     const announcement = await Announcement.findByPk(announcementId);
-    if(announcement === null) {
+    if (announcement === null) {
         throw new NotFoundError();
     }
     return announcement;
@@ -19,13 +27,32 @@ async function getAnnouncements(active) {
     return announcements;
 }
 
-async function addEnrolledDriver({fullName, phoneNumber, carDescription}) {
-    const enrolled = await DriverEnrollment.create({fullName, phoneNumber, carDescription});
+async function registerDevice({ token, platform }) {
+    if(platform === 'iOS') {
+        const params = {
+            Protocol: 'application',
+            TopicArn: 'arn:aws:sns:eu-central-1:872912343417:seaats-marketing',
+            Endpoint: token
+        };
+
+        sns.subscribe(params, (err, data) => {
+            if(err) {
+                console.error('Error subscribing device to SNS: ', err);
+            } else {
+                console.log('Device successfully subscribed to SNS: ', data);
+            }
+        })
+    }
+}
+
+async function addEnrolledDriver({ fullName, phoneNumber, carDescription }) {
+    const enrolled = await DriverEnrollment.create({ fullName, phoneNumber, carDescription });
     return true;
 }
 
 module.exports = {
     getAnnouncement,
     getAnnouncements,
-    addEnrolledDriver
+    addEnrolledDriver,
+    registerDevice
 }
