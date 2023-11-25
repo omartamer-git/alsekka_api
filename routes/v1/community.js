ebe01370fb6c372095caf1d873e5f2c21f1595dd
Omar Abdelnaby <54456013+omartamer-git@users.noreply.github.com>
2023-11-25 12:37:21 +0100

    New structure (#5)
    * New Routing
    
    * Hotfix emails
    
    * Rate Limiting:
    
    * Trust proxy

---
+++ routes/v1/community.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const { authenticateToken } = require('../../middleware/authenticateToken');
+const { BadRequestError } = require('../../errors/Errors');
+const router = express.Router();
+const communityService = require("../../services/communityService");
+
+
+router.post("/createcommunity", authenticateToken, async (req, res, next) => {
+    const { name, description, private, joinQuestion } = req.body;
+    const file = req.files[0];
+    const uid = req.user.userId;
+
+    if (!file || !name || !description || !private || (private === 1 && !joinQuestion)) {
+        return next(new BadRequestError());
+    }
+
+
+    communityService.createCommunity({ name, description, private, joinQuestion }, file, uid).then(community => {
+        return res.json({ success: 1 });
+    }).catch(next);
+});
+
+router.patch("/updatecommunity", authenticateToken, async (req, res, next) => {
+    const { communityId, description, private, joinQuestion } = req.body;
+    const file = req.files[0];
+    const uid = req.user.userId;
+
+    if (!communityId || !description || !private || (private == 1 && !joinQuestion)) {
+        return next(new BadRequestError());
+    }
+
+    communityService.updateCommunity(req.body, file, uid).then(community => {
+        return res.json({ success: 1 });
+    }).catch(next);
+});
+
+router.get("/communities", authenticateToken, async (req, res, next) => {
+    let { page } = req.query;
+    if (!page) {
+        req.query.page = 1;
+    }
+
+    const uid = req.user.userId;
+
+    communityService.getCommunities({ uid, ...req.query }).then(communities => {
+        res.json(communities);
+    }).catch(next);
+});
+
+router.patch("/leavecommunity", authenticateToken, async (req, res, next) => {
+    const { communityId } = req.body;
+
+    if (!communityId) {
+        return next(new BadRequestError());
+    }
+
+    const uid = req.user.userId;
+
+    communityService.leaveCommunity(req.body, uid).then(() => {
+        res.json({ success: 1 });
+    }).catch(next);
+});
+
+router.get("/mycommunities", authenticateToken, async (req, res, next) => {
+    const uid = req.user.userId;
+
+    if (!uid) {
+        return next(new BadRequestError());
+    }
+
+    communityService.getUserCommunities({ uid, ...req.query }).then(communities => {
+        return res.json(communities.Communities);
+    }).catch(next);
+});
+
+router.get("/communitydetails", authenticateToken, async (req, res, next) => {
+    const { communityId } = req.query;
+    const uid = req.user.userId;
+
+    if (!communityId || !uid) {
+        return next(new BadRequestError());
+    }
+
+    communityService.getCommunityDetails({ communityId, uid, ...req.query }).then(community => {
+        return res.json(community);
+    }).catch(next);
+});
+
+router.get("/communitymembers", authenticateToken, async (req, res, next) => {
+    const { communityId } = req.query;
+
+    if (!communityId) {
+        return next(new BadRequestError());
+    }
+
+    communityService.getCommunityMembers(req.query, req.user.userId).then(members => res.json(members)).catch(next);
+});
+
+router.patch("/acceptmember", authenticateToken, async (req, res, next) => {
+    const { memberId } = req.body;
+    if (!memberId) {
+        return next(new BadRequestError());
+    }
+
+    communityService.acceptCommunityMember(req.body, req.user.userId).then(() => res.json({ success: 1 })).catch(next);
+});
+
+router.patch("/rejectmember", authenticateToken, async (req, res, next) => {
+    const { memberId } = req.body;
+    if (!memberId) {
+        return next(new BadRequestError());
+    }
+
+    communityService.rejectCommunityMember(req.body, req.user.userId).then(() => res.json({ success: 1 })).catch(next);
+});
+
+router.get("/myfeed", authenticateToken, async (req, res, next) => {
+    let { page } = req.query;
+    if (!page) { req.query.page = 1; }
+
+    const uid = req.user.userId;
+
+    if (!uid) {
+        return next(new BadRequestError());
+    }
+
+    communityService.getUserFeed({ uid, ...req.query }).then(feed => {
+        return res.json(feed);
+    }).catch(err => {
+        console.error(err);
+        return next(err);
+    });
+});
+
+router.get("/searchcommunities", authenticateToken, async (req, res, next) => {
+    const { name, page } = req.query;
+
+    if (!name) {
+        return next(new BadRequestError());
+    }
+    if (!page) {
+        req.query.page = 1;
+    }
+
+    communityService.searchCommunities({ name, ...req.query }).then(searchResult => {
+        return res.json(searchResult);
+    }).catch(next);
+});
+
+router.post("/joincommunity", authenticateToken, async (req, res, next) => {
+    const { communityId, answer } = req.body;
+    const uid = req.user.userId;
+
+    if (!uid || !communityId) {
+        return next(new BadRequestError());
+    }
+
+    communityService.joinCommunity({ uid, communityId, answer, ...req.body }).then(joinResult => {
+        return res.json(joinResult);
+    }).catch(next);
+});
+
+
+module.exports = router;
---
+++ routes/v1/staff.js
@@ -0,0 +1,196 @@
+const express = require('express');
+const { authenticateToken, sessionChecker } = require('../../middleware/authenticateToken');
+const { BadRequestError } = require('../../errors/Errors');
+const router = express.Router();
+const staffService = require("../../services/staffService");
+
+router.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return next(new BadRequestError());
+    }
+
+    staffLogin(req.body).then(response => {
+        req.session.profile = { loggedIn: true };
+        res.send("{}");
+    }).catch(next);
+});
+
+router.get("/session", sessionChecker, async (req, res, next) => {
+    res.json({ success: 1 });
+});
+
+router.get("/searchuser", sessionChecker, async (req, res, next) => {
+    const { phone } = req.query;
+
+    if (!phone) {
+        return next(new BadRequestError());
+    }
+
+    findUser(req.query).then(user => {
+        res.json(user);
+    }).catch(next);
+});
+
+router.get("/userrides", sessionChecker, async (req, res, next) => {
+    let { uid, page } = req.query;
+
+    if (!uid) {
+        return next(new BadRequestError());
+    }
+
+    if (!page) {
+        page = 1;
+    }
+
+    const limit = 8;
+    const offset = (page - 1) * limit;
+    const after = null;
+    rideService.getPastRides({ uid, limit, after, offset }).then(rides => {
+        res.json(rides);
+    }).catch(next);
+});
+
+router.post("/updateuser", sessionChecker, async (req, res, next) => {
+    if (!req.body.id) {
+        return next(new BadRequestError());
+    }
+
+    updateUser(req.body).then(newUser => res.json(newUser)).catch(next);
+});
+
+router.get("/userlicenses", sessionChecker, async (req, res, next) => {
+    if (!req.query.uid) {
+        return next(new BadRequestError());
+    }
+
+    customerLicenses(req.query).then(licenses => {
+        res.json(licenses);
+    }).catch(next);
+});
+
+router.post("/updatelicense", sessionChecker, async (req, res, next) => {
+    if (!req.body.id) {
+        return next(new BadRequestError());
+    }
+
+    updateLicense(req.body).then(license => {
+        res.json(license);
+    }).catch(next);
+});
+
+router.get("/pendinglicenses", sessionChecker, async (req, res, next) => {
+    getPendingLicenses().then(licenses => {
+        res.json(licenses);
+    }).catch(next);
+});
+
+router.post("/updatecar", sessionChecker, async (req, res, next) => {
+    if (!req.body.id) {
+        return next(new BadRequestError());
+    }
+
+    updateCar(req.body).then(car => {
+        res.json(car);
+    }).catch(next);
+});
+
+router.get("/pendingcars", sessionChecker, async (req, res, next) => {
+    getPendingCars().then(cars => {
+        res.json(cars);
+    }).catch(next);
+});
+
+router.get("/members", sessionChecker, async (req, res, next) => {
+    getMembers().then(members =>
+        res.json(members)
+    ).catch(next);
+});
+
+router.post("/createuser", sessionChecker, async (req, res, next) => {
+    const { username, password, phone, role } = req.body;
+    if (!username || !password || !phone || !role) {
+        return next(new BadRequestError());
+    }
+
+    createStaffMember(req.body).then(() => {
+        res.json({ success: true });
+    }).catch((err) => {
+        console.error(err);
+    });
+});
+
+router.get("/memberdetails", sessionChecker, async (req, res, next) => {
+    const { id } = req.query;
+    if (!id) {
+        next(new BadRequestError());
+    }
+
+    getStaffMember(req.query).then(member => {
+        res.json(member);
+    }).catch(next);
+});
+
+router.post("/editmember", sessionChecker, async (req, res, next) => {
+    if (!req.body.id) {
+        return next(new BadRequestError());
+    }
+
+    console.log(req.body);
+
+    editStaffMember(req.body).then(() => {
+        res.json({ success: 1 });
+    }).catch(next);
+});
+
+router.get("/announcements", sessionChecker, async (req, res, next) => {
+    getAllAnnouncements().then(ann => {
+        res.json(ann);
+    }).catch(next);
+});
+
+router.post("/updateannouncement", sessionChecker, async (req, res, next) => {
+    const { id } = req.body;
+    if (!id) {
+        return next(new BadRequestError());
+    }
+    console.log(req.body);
+    updateAnnouncement(req.body).then(() => {
+        res.json({ success: 1 });
+    }).catch(next);
+});
+
+router.post("/createannouncement", sessionChecker, async (req, res, next) => {
+    const { title_en, title_ar, text_en, text_ar, from, to, active } = req.body;
+
+    if (!title_en || !title_ar || !text_en || !text_ar || !from || !to || !active) {
+        next(new BadRequestError());
+    }
+
+    createAnnouncement(req.body).then(() => {
+        res.json({ success: 1 });
+    }).catch(next);
+});
+
+router.get("/ride", sessionChecker, async (req, res, next) => {
+    if (!req.query.id) {
+        return next(new BadRequestError());
+    }
+
+    getFullRide(req.query).then(ride => {
+        res.json(ride);
+    }).catch(next);
+});
+
+router.post("/cancelride", sessionChecker, async (req, res, next) => {
+    const { id } = req.body;
+    if (!id) {
+        return next(new BadRequestError());
+    }
+
+    cancelRide(req.body).then(() => {
+        res.json({ success: 1 });
+    }).catch(next);
+});
+
+module.exports = router;
