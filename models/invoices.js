41e321641de2a907621ec1c6cedf4f46c03fa891
omartamer-git <omartamer02@hotmail.com>
2023-12-22 04:46:32 +0200

    Invoicing Changes

--- models/invoices.js
+++ models/invoices.js
@@ -19,6 +19,11 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.INTEGER,
             allowNull: false
         },
+        pickupAddition: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
         balanceDue: {
             type: DataTypes.INTEGER,
             allowNull: false,
