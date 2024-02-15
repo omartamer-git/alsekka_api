a7d4a1b7c42f6188b872f3e0efbcc9e674be81a0
omartamer-git <omartamer02@hotmail.com>
2024-02-15 19:50:32 +0200

    fix

--- services/kashierService.js
+++ services/kashierService.js
@@ -1,19 +1,16 @@
 //Copy and paste this code in your Backend
 let crypto = require('crypto');
 function generateKashierOrderHash(passengerId, userId, grandTotal) {
-    const mid = 'MID-123-123'; //your merchant id
-    // const CustomerReference = '1'; //your customer id to save card
-
-    // const amount = order.amount; //eg: 22.00
-    // const currency = order.currency; //eg: "EGP"
-    // const orderId = order.merchantOrderId; //eg: 99
+    console.log(passengerId);
+    console.log(userId);
+    console.log(grandTotal);
+    console.log(hash);
     const secret = process.env.KASHIERAPIKEY;
     const path = `/?payment=${process.env.KASHIER_ID}.${passengerId}.${grandTotal}.EGP${userId ? ('.' + userId) : null}`;
 
     const hash = crypto.createHmac('sha256', secret).update(path).digest('hex');
     return hash;
 }
-//The Result Hash for /?payment=mid-0-1.99.20.EGP with secret 11111 should result 606a8a1307d64caf4e2e9bb724738f115a8972c27eccb2a8acd9194c357e4bec
 
 module.exports = {
     generateKashierOrderHash
