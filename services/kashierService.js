0262253d9dc84c50279415c932aba07c91b8657b
omartamer-git <omartamer02@hotmail.com>
2024-02-15 20:19:46 +0200

    rb

--- services/kashierService.js
+++ services/kashierService.js
@@ -5,7 +5,7 @@ function generateKashierOrderHash(passengerId, userId, grandTotal) {
     console.log(userId);
     console.log(grandTotal);
     const secret = process.env.KASHIERAPIKEY;
-    const path = `/?payment=${process.env.KASHIER_ID}.${passengerId}.${grandTotal}.EGP`;
+    const path = `/?payment=${process.env.KASHIER_ID}.${passengerId}.${grandTotal}.EGP${userId ? ('.' + userId) : null}`;
 
     const hash = crypto.createHmac('sha256', secret).update(path).digest('hex');
     return hash;
