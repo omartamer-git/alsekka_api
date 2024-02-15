bc56a7c2a77aebe45fb98eb148787201171b29fa
omartamer-git <omartamer02@hotmail.com>
2024-02-15 20:44:18 +0200

    Webhook signature validation

--- routes/v1/payment.js
+++ routes/v1/payment.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { default: rateLimit } = require('express-rate-limit');
+const _ = require('underscore');
+const crypto = require('crypto');
+const queryString = require('query-string');
+
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 6015 minutes
     max: 450, // Limit each IP to 450 requests per `window` (here, per 60 minutes)
@@ -12,8 +16,23 @@ router.use(limiter);
 
 
 router.post("/webhook", async (req, res, next) => {
-    console.log(req.body);
-    res.json({});
+    res.status(200).json({});
+
+
+    const { data, event } = req.body;
+    data.signatureKeys.sort();
+    const objectSignaturePayload = _.pick(data, data.signatureKeys);
+    const signaturePayload = queryString.stringify(objectSignaturePayload);
+    const signature = crypto
+      .createHmac('sha256', process.env.KASHIERAPIKEY)
+      .update(signaturePayload)
+      .digest('hex');
+    const kashierSignature = req.header('x-kashier-signature');
+    if (kashierSignature === signature) {
+      console.log('valid signature');
+    } else {
+      console.log('invalid signature');
+    }
 });
 
 
